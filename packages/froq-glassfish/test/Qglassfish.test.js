184703ae4c98ddd6ba8303485232035ca8aef1cb
Philipp Renoth <renoth@wildoak.de>
2018-01-19 22:41:26 +0100

    v0.0.5

--- packages/froq-glassfish/test/Qglassfish.test.js
+++ packages/froq-glassfish/test/Qglassfish.test.js
@@ -1,5 +1,13 @@
 import { test } from 'ava';
+import Qdocker from 'froq-docker';
+
+let docker;
+
+test.before(async () => {
+    docker = Qdocker.fromSocket();
+    await docker.pull('payara/server-full');
+});
 
 test('', t => {
-    t.pass();
+    
 });
